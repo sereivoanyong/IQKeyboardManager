a65c67a73414e634477d21ba63f4707bc1358d8e
Sereivoan Yong <sereivoanyong@gmail.com>
2020-12-13 22:26:08 +0700

    Merge IQKeyboardManager+OrientationNotification.swift into IQKeyboardManager.swift

--- IQKeyboardManagerSwift/IQKeyboardManager.swift
+++ IQKeyboardManagerSwift/IQKeyboardManager.swift
@@ -1314,6 +1314,50 @@ Codeless drop-in universal library allows to prevent issues of keyboard sliding 
         let elapsedTime = CACurrentMediaTime() - startTime
         showLog("****** \(#function) ended: \(elapsedTime) seconds ******", indentation: -1)
     }
+
+    // MARK: - UIStatusBar Notification methods
+
+    /**  UIApplicationWillChangeStatusBarOrientationNotification. Need to set the textView to it's original position. If any frame changes made. (Bug ID: #92)*/
+    @objc func willChangeStatusBarOrientation(_ notification: Notification) {
+
+        let currentStatusBarOrientation: UIInterfaceOrientation
+        if #available(iOS 13, *) {
+            currentStatusBarOrientation = keyWindow()?.windowScene?.interfaceOrientation ?? UIInterfaceOrientation.unknown
+        } else {
+            currentStatusBarOrientation = UIApplication.shared.statusBarOrientation
+        }
+
+        guard let statusBarOrientation = notification.userInfo?[UIApplication.statusBarOrientationUserInfoKey] as? Int, currentStatusBarOrientation.rawValue != statusBarOrientation else {
+            return
+        }
+
+        let startTime = CACurrentMediaTime()
+        showLog("****** \(#function) started ******", indentation: 1)
+
+        //If textViewContentInsetChanged is saved then restore it.
+        if let textView = textFieldView as? UITextView, textView.responds(to: #selector(getter: UITextView.isEditable)) {
+
+            if isTextViewContentInsetChanged {
+                self.isTextViewContentInsetChanged = false
+                if textView.contentInset != self.startingTextViewContentInsets {
+                    UIView.animate(withDuration: animationDuration, delay: 0, options: animationCurve, animations: { () -> Void in
+
+                        self.showLog("Restoring textView.contentInset to: \(self.startingTextViewContentInsets)")
+
+                        //Setting textField to it's initial contentInset
+                        textView.contentInset = self.startingTextViewContentInsets
+                        textView.scrollIndicatorInsets = self.startingTextViewScrollIndicatorInsets
+
+                    }, completion: { (_) -> Void in })
+                }
+            }
+        }
+
+        restorePosition()
+
+        let elapsedTime = CACurrentMediaTime() - startTime
+        showLog("****** \(#function) ended: \(elapsedTime) seconds ******", indentation: -1)
+    }
 }
 
 extension IQKeyboardManager: UIGestureRecognizerDelegate {
